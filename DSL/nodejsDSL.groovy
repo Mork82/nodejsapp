5aa8367daad9640058f5f086f6240ee058f68a1e
Jose Corcoles Contreras <61096874+Mork82@users.noreply.github.com>
2022-12-21 20:07:44 +0100

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Mork82/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('Jose Corcoles')
+            node / gitConfigEmail('joscorconn@gmail.com')
         }
     }
     triggers {
@@ -15,23 +15,6 @@ job('Aplicacion Node.js DSL') {
     steps {
         shell("npm install")
     }
-    publishers {
-	slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-        }
-    }
+    
+    
 }
